00e8ebff0f080239d11d38554abf2679f9691672
Viktor <viktor@intrykk.se>
2020-05-07 00:04:36 +0200

    Upload data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   PersonalDetailsScreen,
   QuestionnaireText,
+  SUSQuestionnaire,
   CompletedMessage,
 } from './components';
 
@@ -10,6 +11,50 @@ export const App = () => {
   const [questionnaireAnswers, setQuestionnaireAnswers] = useState<
     QuestionnaireAnswers
   >();
+  const [susResponse, setSUSResponse] = useState<SUSResponse>();
+
+  const [dataSaved, setDataSaved] = useState<boolean | undefined>(undefined);
+  const [rejectedPayload, setRejectedPayload] = useState<object | undefined>(
+    undefined,
+  );
+
+  useEffect(() => {
+    if (dataSaved) {
+      return;
+    }
+
+    if (personalDetails && questionnaireAnswers && susResponse) {
+      const payload = {
+        response: {
+          personalDetails,
+          questionnaireAnswers,
+          susResponse,
+        },
+      };
+
+      const url =
+        'https://directions-service.azurewebsites.net/api/uploadtextresponse?code=Nsr00Qe2TxgkVJTH4y5mFIcFaomvOsm/jtSYCGbeSkVqcp4C48xxpQ==';
+
+      fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      })
+        .then((response) => {
+          if (response.ok) {
+            setDataSaved(true);
+            setRejectedPayload(undefined);
+          } else {
+            setDataSaved(false);
+            setRejectedPayload(payload);
+          }
+        })
+        .catch(console.warn);
+    }
+  }, [personalDetails, questionnaireAnswers, susResponse, dataSaved]);
 
   if (!personalDetails) {
     return <PersonalDetailsScreen callback={setPersonalDetails} />;
@@ -19,5 +64,9 @@ export const App = () => {
     return <QuestionnaireText callback={setQuestionnaireAnswers} />;
   }
 
-  return <CompletedMessage />;
+  if (!susResponse) {
+    return <SUSQuestionnaire callback={setSUSResponse} />;
+  }
+
+  return <CompletedMessage message={rejectedPayload} />;
 };
--- src/components/CompletedMessage/CompletedMessage.tsx
+++ src/components/CompletedMessage/CompletedMessage.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import { View, Text } from '..';
+import { View, Divider, Text } from '..';
 
-export const CompletedMessage = () => (
+export interface CompletedMessageProps {
+  message?: Object | undefined;
+}
+
+export const CompletedMessage = ({
+  message = undefined,
+}: CompletedMessageProps) => (
   <View type="container">
     <Text type="header">Det var allt!</Text>
     <Text type="onGrayBackground" margin="horizontal">
       Nu är undersökningen klar. Stort tack för din medverkan!
     </Text>
+    {message && (
+      <>
+        <Divider />
+        <Text type="onGrayBackground">{JSON.stringify(message)}</Text>
+      </>
+    )}
   </View>
 );
